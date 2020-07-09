6058b19d8f0335f7a69d5a58bae14e49b3dd7470
Rachel Bennett <rachellaurynbennett@gmail.com>
2020-07-09 15:19:04 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,4 +1,12 @@
+<<<<<<< HEAD
 def parrot(phrase="Squawk!")
   puts "#{phrase}"
   return "#{phrase}"
 end
+=======
+def parrot(name="Pretty Bird")
+  parrot = "Squawk"
+  puts "Squawk!"
+  return "Squawk!"
+end
+>>>>>>> d0dc7ca562220724291494d41cf9cf51084234fa
